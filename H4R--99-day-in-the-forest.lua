498356d0b3878f5da1ba28c90f1ac08f3f9ea779
H4R-hack <scamnot146@gmail.com>
2025-08-21 09:19:59 +0700

    Update H4R--99-day-in-the-forest.lua

--- H4R--99-day-in-the-forest.lua
+++ H4R--99-day-in-the-forest.lua
@@ -1,1 +1,2 @@
-https://raw.githubusercontent.com/H4R-hack/H4R-0--0-1.1.1-/refs/heads/main/99%20day%20in%20the%20forest.Lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/H4R-hack/H4R-0--0-1.1.1-/refs/heads/main/99%20day%20in%20the%20forest.Lua"))()
+
